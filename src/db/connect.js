1923a31156cd6f27f93f0df80af4b4bb47ad2f32
Prasad <prasadwakle026@gmail.com>
2022-12-16 22:31:29 +0530

    Added connection string

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/taskDB")
+mongoose.connect(process.env.CUSTOMCONNSTR_MyConnectionString||"mongodb://localhost:27017/taskDB")
 .then(() => {
     console.log("Connection Successful");
 }).catch((e) =>{
